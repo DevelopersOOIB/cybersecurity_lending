cb7589ce67dcd85d9b688c7f178ac5e459542e3f
MrNansy <galimov.ad@dvfu.ru>
2023-10-26 12:29:35 +1000

    update program

--- js/full__program.js
+++ js/full__program.js
@@ -2,17 +2,8 @@ function downloadFile(url, filename) {
     let link = document.getElementById('download');
     link.href = url;
     link.download = filename;
-
-    // Делаем ссылку невидимой
-    // link.style.display = 'none';
-
-    // document.body.appendChild(link);
-
-    // // Удаляем ссылку из DOM
-    // document.body.removeChild(link);
 }
 
-// Привязываем функцию к событию click на кнопке
 document.getElementById('download').addEventListener('click', function() {
     downloadFile('download/program.pdf', 'program');
 });
