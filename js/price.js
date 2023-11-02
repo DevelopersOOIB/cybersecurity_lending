2b09cd162dd20e517243dd53955f9b2a4e92527f
MrNansy <galimov.ad@dvfu.ru>
2023-11-02 17:24:54 +1000

    update js price

--- js/price.js
+++ js/price.js
@@ -1,13 +1,30 @@
 let priceMoneys = "80 000 рублей";
 let discountedPrice = "64 000 рублей";
-let discountedEndDate = new Date("2023-10-20");
+let discountedDate = "до 05.11.2023";
+let discountedEndDate = new Date("2023-11-07");
 
 let curentDate = new Date();
 
-function updatePrice(elementId, price, discountPrice, endDate, currentDate) {
-    document.getElementById(`priceMoney${elementId}`).textContent = price;
-    document.getElementById(`discountedPrice${elementId}`).textContent = currentDate < endDate ? discountPrice : "";
+function updatePrice(elementId, price, discountPrice, discountDate, endDate, currentDate) {
+    if (currentDate < endDate) {
+        let Money = document.getElementById(`priceMoney${elementId}`);
+        let Discount =  document.getElementById(`discountedPrice${elementId}`);
+        let discountedDate = document.getElementById(`discountedDate${elementId}`);
+        Money.textContent = price;
+        Money.style = 'font-size: 150%; text-decoration: line-through; text-decoration-color: red; text-decoration-thickness: 0.4rem; margin-bottom: 0';
+        Discount.textContent = discountPrice;
+        Discount.style = 'font-size: 200%; font-family: "Gotham Pro Black", sans-serif;';
+        discountedDate.textContent = discountDate;
+        discountedDate.style = 'font-size: 150%; font-family: "Gotham Pro Light", sans-serif; margin-bottom: 5%';
+    } else {
+        let Money = document.getElementById(`priceMoney${elementId}`);
+        let Discount =  document.getElementById(`discountedPrice${elementId}`);
+        let discountedDate = document.getElementById(`discountedDate${elementId}`);
+        Money.textContent = price;
+        // Discount.textContent = "";
+        // discountedDate.textContent = "";
+    }
 }
 
-updatePrice('1', priceMoneys, discountedPrice, discountedEndDate, curentDate);
-updatePrice('2', priceMoneys, discountedPrice, discountedEndDate, curentDate);
+updatePrice('1', priceMoneys, discountedPrice, discountedDate, discountedEndDate, curentDate);
+updatePrice('2', priceMoneys, discountedPrice, discountedDate, discountedEndDate, curentDate);
