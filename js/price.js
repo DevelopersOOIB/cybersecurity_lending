41d7c37837720d2afa59e3a338c347dac5461298
MrNansy <galimov.ad@dvfu.ru>
2023-11-03 17:41:42 +1000

    update

--- js/price.js
+++ js/price.js
@@ -1,6 +1,6 @@
 let priceMoneys = "80 000 рублей";
 let discountedPrice = "64 000 рублей";
-let discountedDate = "до 05.11.2023";
+let discountedDate = "до 07.11.2023";
 let discountedEndDate = new Date("2023-11-07");
 
 let curentDate = new Date();
@@ -10,19 +10,20 @@ function updatePrice(elementId, price, discountPrice, discountDate, endDate, cur
         let Money = document.getElementById(`priceMoney${elementId}`);
         let Discount =  document.getElementById(`discountedPrice${elementId}`);
         let discountedDate = document.getElementById(`discountedDate${elementId}`);
-        Money.textContent = price;
-        Money.style = 'font-size: 150%; text-decoration: line-through; text-decoration-color: red; text-decoration-thickness: 0.4rem; margin-bottom: 0';
+        Money.textContent = price;        
+        Money.style = 'font-size: max(200%);';
         Discount.textContent = discountPrice;
-        Discount.style = 'font-size: 200%; font-family: "Gotham Pro Black", sans-serif;';
         discountedDate.textContent = discountDate;
-        discountedDate.style = 'font-size: 150%; font-family: "Gotham Pro Light", sans-serif; margin-bottom: 5%';
     } else {
         let Money = document.getElementById(`priceMoney${elementId}`);
         let Discount =  document.getElementById(`discountedPrice${elementId}`);
         let discountedDate = document.getElementById(`discountedDate${elementId}`);
         Money.textContent = price;
-        // Discount.textContent = "";
-        // discountedDate.textContent = "";
+        Money.classList.remove(`strike-through${elementId}`);
+        Discount.textContent = "";
+        Discount.classList.remove(`discountedPrice`);
+        discountedDate.textContent = "";
+        discountedDate.classList.remove(`discountedDate`);
     }
 }
 
