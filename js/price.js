68f34d06060c7b3a89ff846aea5389f744b2ec41
MrNansy <galimov.ad@dvfu.ru>
2023-11-14 15:36:00 +1000

    update 5 files

--- js/price.js
+++ js/price.js
@@ -1,6 +1,6 @@
-let priceMoneys = "80 000 рублей";
-let discountedPrice = "64 000 рублей";
-let discountedDate = "до 31.12.2023";
+let priceMoneys = "₽80 000";
+let discountedPrice = "₽64 000";
+let discountedDate = "предложение действует до 31.12.2023";
 let discountedEndDate = new Date("2023-12-31");
 
 let curentDate = new Date();
@@ -11,15 +11,16 @@ function updatePrice(elementId, price, discountPrice, discountDate, endDate, cur
         let Discount =  document.getElementById(`discountedPrice${elementId}`);
         let discountedDate = document.getElementById(`discountedDate${elementId}`);
         Money.textContent = price;        
-        Money.style = 'font-size: max(200%);';
+        Money.style = 'text-decoration: line-through; font-size: max(150%); font-family: "Gotham Pro Light", sans-serif;';
         Discount.textContent = discountPrice;
         discountedDate.textContent = discountDate;
+        discountedDate.style = 'font-size: max(100%); text-transform: lowercase; font-family: "Gotham Pro Light", sans-serif;';
     } else {
         let Money = document.getElementById(`priceMoney${elementId}`);
         let Discount =  document.getElementById(`discountedPrice${elementId}`);
         let discountedDate = document.getElementById(`discountedDate${elementId}`);
         Money.textContent = price;
-        Money.classList.remove(`strike-through${elementId}`);
+        // Money.classList.remove(`strike-through${elementId}`);
         Discount.textContent = "";
         Discount.classList.remove(`discountedPrice`);
         discountedDate.textContent = "";
