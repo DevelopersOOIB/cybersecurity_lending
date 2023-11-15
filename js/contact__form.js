733dc10949d74ef33678025f38238335041d5e00
MrNansy <galimov.ad@dvfu.ru>
2023-11-15 16:35:09 +1000

    update index.html, contact__form.js and mail.php

--- js/contact__form.js
+++ js/contact__form.js
@@ -1,31 +1,15 @@
-var form = document.getElementById('form');
-form.addEventListener('submit', function(event) {
- event.preventDefault();
-
- let name = document.getElementById('name').value;
- let phone = document.getElementById('phone').value;
- let email = document.getElementById('email').value;
- let promocod = document.getElementById('promocod').value;
-
- const formData = {
-    "name": name,
-    "phone": phone,
-    "email": email,
-    "promocod": promocod
- };
-
- fetch('http://10.60.18.13:3000/', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(formData)
- })
-    .then(data => data.json())
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(this);
+  fetch(this.action, {
+      method: 'POST',
+      body: formData
+  })
+  .then(response => response.text())
+  .then(data => {
+      if (data === 'success') {
+      } else {
+          alert('Ошибка: ' + data);
+      }
+  });
 });
-
-// http://10.60.18.13:3000/
-
-// https://webhook.site/6eecd3b7-3474-466d-b00a-6fcd7c4660c5
--- php/mail.php
+++ php/mail.php
@@ -3,34 +3,35 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
- 
-$mail = new PHPMailer();
-$mail->CharSet = 'utf-8';
-$name = $_POST['form__name'];
-$phone = $_POST['form__phone'];
-$email = $_POST['form__email'];
-$promocod = $_POST['form__promocod'];
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   $mail = new PHPMailer();
+   $mail->CharSet = 'utf-8';
+   $name = $_POST['form__name'];
+   $phone = $_POST['form__phone'];
+   $email = $_POST['form__email'];
+   $promocod = $_POST['form__promocod'];
 
-$mail->isSMTP();    
-$mail->Host   = 'smtp.yandex.ru';
-$mail->SMTPAuth   = true; 
-$mail->Username   = 'mail';
-$mail->Password   = 'password';  
-$mail->SMTPSecure = 'ssl';        
-$mail->Port   = 465;               
- 
-$mail->setFrom('noreply_dpo-ib@dvfu.ru', 'noreply'); 
-$mail->addAddress('dpo-ib@dvfu.ru', 'ДПО');
- 
-$mail->Subject = 'Заявка на курс Администрирование и безопасность компьютерных систем';
-$mail->Body = '' .$name . '' .$phone . '' .$email . '' .$promocod . '';
-$mail->AltBody = '';
-                
-if ($mail->send()) {
-  echo '';
-} else {
-  echo 'Ошибка: ' . $mail->ErrorInfo;
+   $mail->isSMTP();   
+   $mail->Host  = 'smtp.yandex.ru';
+   $mail->SMTPAuth  = true; 
+   $mail->Username  = 'mail';
+   $mail->Password  = 'password'; 
+   $mail->SMTPSecure = 'ssl';       
+   $mail->Port  = 465;              
+
+   $mail->setFrom('noreply_dpo-ib@dvfu.ru', 'noreply'); 
+   $mail->addAddress('dpo-ib@dvfu.ru', 'ДПО');
+
+   $mail->Subject = 'Заявка на курс Администрирование и безопасность компьютерных систем';
+   $mail->Body = '' .$name . '' .$phone . '' .$email . '' .$promocod . '';
+   $mail->AltBody = '';
+               
+   if ($mail->send()) {
+       echo 'success';
+   } else {
+       echo 'Ошибка: ' . $mail->ErrorInfo;
+   }
+   exit;
 }
-
 ?>
