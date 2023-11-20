75700cd528598f4f70b6260ad62691dce3123446
MrNansy <galimov.ad@dvfu.ru>
2023-11-20 12:47:45 +1000

    update 14 files

--- php/mail.php
+++ php/mail.php
@@ -1,38 +1,52 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-require 'C:/OSPanel/domains/localhost/vendor/autoload.php'; // require __DIR__ . '/../vendor/autoload.php';
-
+require __DIR__ . '/../vendor/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   $mail = new PHPMailer();
-   $mail->CharSet = 'utf-8';
-   $name = $_POST['form__name'];
-   $phone = $_POST['form__phone'];
-   $email = $_POST['form__email'];
-   $promocod = $_POST['form__promocod'];
+ $mail = new PHPMailer();
+ $mail->CharSet = 'utf-8';
 
-   $mail->isSMTP();
-   $mail->Host  = 'ssl://smtp.yandex.ru';        
-   $mail->Port  = 465;
-   $mail->SMTPAuth  = true; 
-   $mail->Username  = 'mail';
-   $mail->Password  = 'pass';            
+ if(!empty($_POST['form__name1']) && !empty($_POST['form__phone1']) && !empty($_POST['form__email1'])){
+  $name = $_POST['form__name1'];
+  $phone = $_POST['form__phone1'];
+  $email = $_POST['form__email1'];
+  $promocod = $_POST['form__promocod1'];
+ } elseif(!empty($_POST['form__name2']) && !empty($_POST['form__phone2']) && !empty($_POST['form__email2'])){
+  $name = $_POST['form__name2'];
+  $phone = $_POST['form__phone2'];
+  $email = $_POST['form__email2'];
+  $promocod = $_POST['form__promocod2'];
+ } else {
+  echo 'Ошибка: обе формы пустые';
+  exit;
+ }
 
-   $mail->isHTML(true);
+ $mail->isSMTP();
+ $mail->Host = 'ssl://smtp.yandex.ru';      
+ $mail->Port = 465;
+ $mail->SMTPAuth = true; 
+ $mail->Username = 'mail';
+ $mail->Password = 'йцукен';          
 
-   $mail->setFrom('mail', 'Заявка ASCS');
-   $mail->addAddress('mail', 'ДПО');
+ $mail->isHTML(true);
 
-   $mail->Subject = 'Заявка на курс Администрирование и безопасность компьютерных систем';
-   $mail->Body = 'Имя: <b>' .$name . '</b><br>Номер телефона: <b>' .$phone . '</b><br>Почта: <b>' .$email . '</b><br>Промокод: <b>' .$promocod . '</b>';
-   $mail->AltBody = $name . "\n" . $phone . "\n" . $email . "\n" . $promocod;
-               
-   if ($mail->send()) {
-       echo 'success';
-   } else {
-       echo 'Ошибка: ' . $mail->ErrorInfo;
-   }
-   exit;
+ $mail->setFrom('dpo-ib@dvfu.ru', 'Заявка ASCS');
+ $mail->addAddress('dpo-ib@dvfu.ru', 'ДПО');
+
+ $mail->Subject = 'Заявка на курс Администрирование и безопасность компьютерных систем';
+ $mail->Body = 'Имя: <b>' .$name . '</b><br>Номер телефона: <b>' .$phone . '</b><br>Почта: <b>' .$email . '</b><br>Промокод: <b>' .$promocod . '</b>';
+ $mail->AltBody = $name . "\n" . $phone . "\n" . $email . "\n" . $promocod;
+             
+ try {
+     if ($mail->send()) {
+         echo 'success';
+     } else {
+         echo 'Ошибка: ' . $mail->ErrorInfo;
+     }
+ } catch (Exception $e) {
+     echo 'Ошибка: ' . $e->getMessage();
+ }
+ exit;
 }
 ?>
